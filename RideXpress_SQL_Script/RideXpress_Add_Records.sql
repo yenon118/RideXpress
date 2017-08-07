15cfee25764d3a3dc6351d10ed9b377f55e1069e
Yen C <yenon118@gmail.com>
2017-08-06 19:01:29 -0500

    found new way to insert date time

--- RideXpress_SQL_Script/RideXpress_Add_Records.sql
+++ RideXpress_SQL_Script/RideXpress_Add_Records.sql
@@ -1,5 +1,14 @@
 USE RideXpress
+
 SELECT * FROM Car
+
+
 INSERT INTO IncidentReport (CarID, DateOfIncident, ReportName, ReportDescription, DateOfReport) 
 VALUES (6, '20120618 10:34:09 AM', 'Jacky', 'Tires puncture causing highway accident', '20120619 10:00:00 AM')
+
+
+INSERT INTO IncidentReport (CarID, DateOfIncident, ReportName, ReportDescription, DateOfReport) 
+VALUES (6, '8/6/2016 9:48 PM', 'Rocky', 'Tires puncture causing highway accident', '20170619 10:00:00 AM')
+
+
 SELECT * FROM IncidentReport
